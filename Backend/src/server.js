a81df2b28533b4b30f24f3f45135c77d2927d355
Aman69 <amansingh.aa4654@gmail.com>
2026-06-15 10:08:03 +0530

    Setup Express backend foundation

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
     res.send("Welcome to Aethra Backend");

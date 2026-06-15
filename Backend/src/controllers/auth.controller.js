4a520f5987f7303d64ceb9f3f435222392826712
Aman69 <amansingh.aa4654@gmail.com>
2026-06-15 20:07:22 +0530

    Implement user registration and database integration

---
+++ Backend/src/controllers/auth.controller.js
@@ -0,0 +1,57 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const validator = require("validator");
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid email",
+      });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
+
+module.exports = {
+  registerUser,
+};
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,3 +1,4 @@
+const authRoutes = require("./routes/authRoutes");
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
@@ -7,6 +8,9 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 

515fac573d5e95e41c253297df9ec2c26aaef600
Aman69 <amansingh.aa4654@gmail.com>
2026-06-29 16:03:12 +0530

    Connect frontend to production backend

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://aethra-5hi1.onrender.com/api",
 });
 
 api.interceptors.request.use(
